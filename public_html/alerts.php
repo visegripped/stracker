a84ed874f93e5d9b0535a210562de02f1a962c3f
visegripped <jt@visegripped.com>
2021-12-11 15:33:32 -0800

    add alerts

---
+++ public_html/alerts.php
@@ -0,0 +1,8 @@
+<?php
+include "secretSauce.php";
+
+function alertsforDay($data) {
+    return getAlertStatusForDay($data);
+}
+
+?>
--- public_html/getDataFromHistory.php
+++ public_html/getDataFromHistory.php
@@ -1,42 +1,54 @@
 <?php
+include "alerts.php";
 
 function calcMA($data, $n) {
     return round(array_sum(array_slice($data, -$n, $n)) / $n, 2);
 }
 
-function calcDelta($data, $n) {
-
-}
-
-function getDataFromHistory($data) {
-    $dataCount = sizeOf($data);
+function getDataFromHistory($history) {
+    $historyCount = sizeOf($history);
     $eodPrices = array();
     $deltas = array();
-    echo "<table><tr><th>Date</th><th>EOD</th><th>MA 20</th><th>MA 50</th><th>Delta</th><th>Delta MA 5</th><th>Delta MA 10</th><th>Delta MA 20</th></tr>";
+    $theData = array();
     // skip the header row.
-    for($i = 1; $i < $dataCount; $i++ ) {
-        $row = $data[$i];
+    for($i = 1; $i < $historyCount; $i++ ) {
+        $row = $history[$i];
         $date = $row[0];
-        $eod = round($row[4], 2);
+        $eod = $row[4];
         array_push($eodPrices, $eod);
 
         if($i > 20) {
             $MA20 = calcMA($eodPrices, 20);
-            $delta = round($eod - $MA20, 2);
+            $delta = $eod - $MA20;
             array_push($deltas, $delta);
         }
         if($i > 25) {
-            $deltaMA5 = calcMA($deltas, 5);
-            $deltaMA10 = calcMA($deltas, 10);
-            $deltaMA20 = calcMA($deltas, 20);
+            $deltaMA5 = calcMA($deltas, 5); //D1
+            $deltaMA10 = calcMA($deltas, 10); //D2
+            $deltaMA20 = calcMA($deltas, 20); //D3
+            $M1 = $MA20 + $deltaMA5; // MA20 + D1
+            $M2 = $MA20 + $deltaMA10; // MA20 + D2
+            $M3 = $MA20 + $deltaMA20; // MA20 + D3
         }
         if($i > 50) {
             $MA50 = calcMA($eodPrices, 50);
         }
-        
-        echo "<tr><td>$date</td><td>$eod</td><td>$MA20</td><td>$MA50</td><td>$delta</td><td>$deltaMA5</td><td>$deltaMA10</td><td>$deltaMA20</td></tr>";
+        $todaysData = array(
+            "date" => $date,
+            "eod" => $eod,
+            "delta" => $delta,
+            "deltaMA5" => $deltaMA5,
+            "deltaMA10" => $deltaMA10,
+            "deltaMA20" => $deltaMA20,
+            "MA20" => $MA20,
+            "MA50" => $MA50,
+            "M1" => $M1,
+            "M2" => $M2,
+            "M3" => $M3,
+        );       
+        $todaysData = array_merge($todaysData, alertsForDay($todaysData));
+        array_push($theData, $todaysData);
     }
-    echo "</table>";
+    return $theData;
 }
-
 ?>
