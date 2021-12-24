d90a56cf76942a5812baab1028d1401525e6d758
visegripped <jt@visegripped.com>
2021-12-24 14:10:25 -0800

    create api.php and return some JSON

--- doc_root/api.php
+++ doc_root/api.php
@@ -1,6 +1,6 @@
 <?php
 include "includes/stracker/mysql.php";
-header('Content-Type: application/json; charset=utf-8');
+// header('Content-Type: application/json; charset=utf-8');
 function getHistory($symbol, $maxRows = 50, $pdo) {
     $history = array();
     $query = "select date, EOD, MA20, MA50, delta, deltaMA5, deltaMA10, deltaMA20, p0, P1, P2, M1, M2, M3 from $symbol ORDER by date DESC LIMIT $maxRows";
@@ -11,6 +11,6 @@ $db = dbConnect();
 $data = getHistory('AAPL', 100, $db);
 $data = array_reverse($data);
 $data = json_encode($data);
-
+header('Content-type: application/json');
 print($data);
 ?>
