5a75a0861c907d1bf299f1bc0738f01591d33967
visegripped <jt@visegripped.com>
2022-01-07 08:48:55 -0800

    Messages can now be cleared individually

--- doc_root/src/components/Messages/Messages.js
+++ doc_root/src/components/Messages/Messages.js
@@ -1,25 +1,48 @@
 import React, { useContext } from "react";
 import { AppContext } from "../../context/AppContext";
-import './Messages.styles.css';
+import "./Messages.styles.css";
 
-export const MessageTemplate = ({ classification, message}) => {
-  return <div className={`message ${classification}`}>
+export const MessageTemplate = ({ classification, message, messageIndex, buttonOnclick }) => {
+  const handleClick = () => {
+    buttonOnclick(messageIndex);
+  }
+  return (
+    <div className={`message ${classification}`}>
       <pre className="message--pre">{message}</pre>
-    <button className={`button message--button message--button--${classification}`}>X</button>
+      <button
+        className={`button message--button message--button--${classification}`}
+        onClick={handleClick}
+      >
+        X
+      </button>
     </div>
-}
+  );
+};
 
 export const Messages = () => {
   const [App, setApp] = useContext(AppContext);
   const { messages } = App;
-  const messageList = []
+  const messageList = [];
 
-  messages.forEach(({ message, classification }) => {
-    messageList.push(<MessageTemplate message={message} classification={classification} />);
-  })
-    return <section className="messagesContainer">
-      {messageList}
-    </section>
-}
+  const removeMessage = (index) => {
+    if(index > -1) {
+      messages.splice(index, 1);
+    }
+    setApp('messages', messages);
+  };
+
+  messages.forEach(({ message, classification }, index) => {
+    messageList.push(
+      <MessageTemplate
+        messageIndex={index}
+        key={`messageId-${index}`}
+        message={message}
+        classification={classification}
+        buttonOnclick={removeMessage}
+      />
+    );
+  });
+  return <section className="messagesContainer">{messageList}</section>;
+};
 
 export default Messages;
