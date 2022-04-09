63e1b040373ee5b6c7d1e165ecf5ae1603d29e0a
visegripped <jt@visegripped.com>
2022-04-09 08:42:35 -0700

    Fix date query and protect against sql injection

--- doc_root/public_html/stracker/api.php
+++ doc_root/public_html/stracker/api.php
@@ -10,7 +10,7 @@ $endDate = $_GET['endDate'];
 
 function getHistory($symbol, $startDate, $endDate, $pdo) {
     $history = array();
-    $query = "select date, EOD, MA20, MA50, delta, deltaMA5, deltaMA10, deltaMA20, P0, P1, P2, M1, M2, M3 from $symbol where date >= $startDate and date <= $endDate order by date DESC";
+    $query = "select date, EOD, MA20, MA50, delta, deltaMA5, deltaMA10, deltaMA20, P0, P1, P2, M1, M2, M3 from $pdo->quote($symbol) where date between $pdo->quote($startDate) and $pdo->quote($endDate) order by date DESC";
     $stmt = $pdo->query($query);
     return $stmt->fetchAll(\PDO::FETCH_ASSOC);
 }
