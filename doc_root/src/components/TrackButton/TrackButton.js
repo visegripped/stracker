c8ba46347256bad32591078a7b8d8a29c93a63ce
visegripped <jt@visegripped.com>
2022-06-26 11:41:41 -0700

    Track button is now smarter

--- doc_root/src/components/TrackButton/TrackButton.js
+++ doc_root/src/components/TrackButton/TrackButton.js
@@ -1,32 +1,45 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { AppContext } from "../../context/AppContext";
 import useMessaging from "../../hooks/useMessaging";
 import apiEndpoints from "../../endpoints.json";
 import "./styles.css";
 
-export const TrackButton = ({ symbol = '', trackedSymbols = [] }) => {
+export const TrackButton = ({ symbol = '' }) => {
   const { addMessage } = useMessaging();
   const [App] = useContext(AppContext);
   const { tokenId, userId } = App;
+  const { trackedSymbols, setTrackedSymbols } = useState([]);
 
   const getTrackedSymbols = () => {
     let formData = new FormData();
     formData.append("tokenId", tokenId);
     formData.append("task", "getTrackedSymbols");
     formData.append("userId", userId);
-    fetch(apiEndpoints.root, {
+    return fetch(apiEndpoints.root, {
       body: formData,
       method: 'post',
-    });
+    })  
+    .then((response) => response.json())
+    .then((data) => {
+      setTrackedSymbols(data);
+    })
+    .catch((err) => {
+      addMessage({
+        message: `Error requesting alerts: ${err}`,
+        classification: "error",
+      });
+    });;
   }
-  getTrackedSymbols();
+  getTrackedSymbols()
 
-  const trackSymbol = () => {
+
+  const handleClick = (clickEvent) => {
     console.log(`-> trackSymbol was triggered for ${symbol}. tokenId is set: ${!!(tokenId)} and userId: ${userId}`);
+    const trackStatus = clickEvent.target.dataset.action || 'track';
     if(tokenId && userId) {
       let formData = new FormData();
       formData.append("tokenId", tokenId);
-      formData.append("task", "track");
+      formData.append("task", trackStatus);
       formData.append("symbol", symbol);
       formData.append("userId", userId);
       fetch(apiEndpoints.root, {
@@ -35,6 +48,7 @@ export const TrackButton = ({ symbol = '', trackedSymbols = [] }) => {
       })
       .then((response) => {
         if (response.status >= 200 && response.status < 300) {
+          getTrackedSymbols();
           return response.json();
         } else {
           addMessage({
@@ -48,7 +62,7 @@ export const TrackButton = ({ symbol = '', trackedSymbols = [] }) => {
           addMessage({ message: data.err, classification: "error" });
         } else {
         // success.  Throw success message.
-          addMessage({ message: `Symbol ${symbol} is now being tracked`, classification: "info" });
+          addMessage({ message: `Symbol ${symbol} is track status changed to: ${trackStatus}`, classification: "info" });
         }
       })
       .catch((err) => {
@@ -59,12 +73,13 @@ export const TrackButton = ({ symbol = '', trackedSymbols = [] }) => {
       });
     }
   }
-  const buttonPrompt = (trackedSymbols.indexOf(symbol) > -1) ? 'Track' : 'Untrack';
+  const buttonAction = (trackedSymbols?.indexOf(symbol) > -1) ? 'Track' : 'Untrack';
   return  (
     <button
-      onClick={trackSymbol}
+      onClick={handleClick}
+      data-action={buttonAction.toLocaleLowerCase()}
       className="trackButton"
-    >{buttonPrompt} {symbol}</button>
+    >{buttonAction} {symbol}</button>
   ) 
 };
 
