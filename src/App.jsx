f644d4fc92508470fb9fb75d87d138b7996a704d
visegripped <jt@visegripped.com>
2024-07-24 20:42:09 -0700

    Add support for a light theme

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useContext, useState, useEffect } from "react";
 import { HashRouter as Router, Route, Routes, Link } from "react-router-dom";
 import Home from "@pages/Home";
 import Alerts from "@pages/Alerts";
@@ -45,67 +45,104 @@ const Notifications = () => {
   );
 };
 
+const setThemeOnBody = (theme) => {
+  const body = document.body;
+  if(body.classList.value) {
+    body.classList.replace(body.classList.value, theme);
+  } else {
+    body.classList.add(theme);
+  }
+  
+}
+
 const App = () => {
   let currentDate = new Date();
   const { accessToken } = useContext(AuthContext);
+  const selectedThemeAtLoad = localStorage.getItem("theme") || "default";
+  const [theme, setTheme] = useState(selectedThemeAtLoad);
+
+  useEffect(() => {
+    if (theme) {
+      setThemeOnBody(theme);
+    }
+  }, [theme]);
+
+  const updateTheme = () => {
+    const newTheme = theme === "default" ? "akira" : "default";
+    localStorage.setItem('theme', newTheme);
+    setTheme(newTheme);
+    setThemeOnBody(newTheme);
+  };
+
   return (
     <Router>
+      <header>
+        <h1 className="logo">
+          <Link to={PathConstants.HOME}>Stracker</Link>
+        </h1>
 
-          <header>
-            <h1 className="logo">
-              <Link to={PathConstants.HOME}>Stracker</Link>
-            </h1>
+        <nav className="navbar">
+          <ul className="nav-list">
+            <li className="nav-item">
+              <Link to={PathConstants.SYMBOL}>Symbol</Link>
+            </li>
+            <li className="nav-item">
+              <Link to={PathConstants.ALERTS}>Alert History</Link>
+            </li>
+          </ul>
+        </nav>
 
-            <nav className="navbar">
-              <ul className="nav-list">
-                <li className="nav-item">
-                  <Link to={PathConstants.SYMBOL}>Symbol</Link>
-                </li>
-                <li className="nav-item">
-                  <Link to={PathConstants.ALERTS}>Alert History</Link>
-                </li>
-              </ul>
-            </nav>
+        <div className="auth">
+          <AuthButton />
+        </div>
+      </header>
 
-            <div className="auth">
-              <AuthButton />
+      <main>
+        <ErrorBoundary
+          fallbackRender={fallbackRender}
+          onError={logError}
+          onReset={(details) => {
+            // Reset the state of your app so the error doesn't happen again - NEED TO EXPLORE THIS
+          }}
+        >
+          <Notifications />
+          {accessToken ? (
+            <Routes>
+              <Route path={PathConstants.HOME} Component={Home} />
+              <Route path={PathConstants.ALERTS} Component={Alerts} />
+              <Route path={PathConstants.SYMBOL} Component={Symbol} />
+              <Route
+                path={`${PathConstants.SYMBOL}/:symbol`}
+                Component={Symbol}
+              />
+              <Route Component={Page404} />
+            </Routes>
+          ) : (
+            <div className="unauthenticated">
+              <h2>You are not logged in.</h2>
+              <h3>Please use the sign in button in the upper right corner.</h3>
             </div>
-          </header>
+          )}
+        </ErrorBoundary>
+      </main>
 
-          <main>
-          <ErrorBoundary
-              fallbackRender={fallbackRender}
-              onError={logError}
-              onReset={(details) => {
-                // Reset the state of your app so the error doesn't happen again - NEED TO EXPLORE THIS
-              }}
-            >
-              <Notifications />
-              {accessToken ? (
-                <Routes>
-                  <Route path={PathConstants.HOME} Component={Home} />
-                  <Route path={PathConstants.ALERTS} Component={Alerts} />
-                  <Route path={PathConstants.SYMBOL} Component={Symbol} />
-                  <Route
-                    path={`${PathConstants.SYMBOL}/:symbol`}
-                    Component={Symbol}
-                  />
-                  <Route Component={Page404} />
-                </Routes>
-              ) : (
-                <div className='unauthenticated'>
-                  <h2>You are not logged in.</h2>
-                  <h3>Please use the sign in button in the upper right corner.</h3>
-                </div>
-              )}
-            </ErrorBoundary>
-          </main>
-
-          <footer>
-            &copy; Copyright 2018 - {currentDate.getFullYear()}. All rights
-            reserved.
-          </footer>
-
+      <footer>
+        <div>
+          &copy; Copyright 2018 - {currentDate.getFullYear()}. All rights
+          reserved.
+        </div>
+        <div>
+          <a
+            href="#"
+            onClick={(e) => {
+              e.preventDefault();
+              updateTheme();
+            }}
+          >
+            Use {theme === "default" ? "akira" : "default"} theme
+          </a>
+        </div>
+      </footer>
     </Router>
   );
 };
